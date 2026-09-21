4a4d6e8b1eb2922d129ef752094a8259988f38bd
Kamlesh Jayasankar <j.kamlesh13042007@gmail.com>
2026-09-21 12:26:58 +0530

    Remove files from repository

--- frontend/src/services/analytics.js
+++
@@ -1,6 +0,0 @@
-import { apiClient } from "./api";
-
-export async function getAnalyticsOverview(days = 30) {
-  const { data } = await apiClient.get("/api/v1/analytics/overview", { params: { days } });
-  return data;
-}
